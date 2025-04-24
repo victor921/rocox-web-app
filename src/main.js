0d9b7955abf51a7b49e1ae2f3e71efc40ea0cb0b
victor921 <victor.olazabal@knights.ucf.edu>
2025-04-23 22:18:29 -0400

    enabling mobile

--- src/main.js
+++ src/main.js
@@ -16,12 +16,6 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // Replace with your actual Firebase config from Firebase Console
 const firebaseConfig = {
-    apiKey: "AIzaSyA1A4Qv-LRp7ytDv-At6lFtWNeGx_apH9c",
-    authDomain: "rocox-136c9.firebaseapp.com",
-    projectId: "rocox-136c9",
-    storageBucket: "rocox-136c9.firebasestorage.app",
-    messagingSenderId: "39493534751",
-    appId: "1:39493534751:web:542c196511d920516a812a",
     measurementId: "G-ZHMH585N3J"
   };
 
@@ -41,7 +35,7 @@ AOS.init({
   once: true,
   offset: 50,
   delay: 100,
-  disable: 'mobile', // Disable animations on mobile (<768px)
+  disable: 'false'
 })
 
 // --- Mount App ---
