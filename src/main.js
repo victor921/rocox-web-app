71a2a5815872729c0f953f574783fd06cd7efad6
victor921 <victor.olazabal@knights.ucf.edu>
2025-04-23 22:31:46 -0400

    fixed config

--- src/main.js
+++ src/main.js
@@ -16,6 +16,12 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // Replace with your actual Firebase config from Firebase Console
 const firebaseConfig = {
+    apiKey: "AIzaSyA1A4Qv-LRp7ytDv-At6lFtWNeGx_apH9c",
+    authDomain: "rocox-136c9.firebaseapp.com",
+    projectId: "rocox-136c9",
+    storageBucket: "rocox-136c9.firebasestorage.app",
+    messagingSenderId: "39493534751",
+    appId: "1:39493534751:web:542c196511d920516a812a",
     measurementId: "G-ZHMH585N3J"
   };
 
