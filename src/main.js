e01bf05ec8a93e4e2deeb49f5de32fd8456cd47a
victor921 <victor.olazabal@knights.ucf.edu>
2025-04-26 13:51:39 -0400

    finalizing changes

--- src/main.js
+++ src/main.js
@@ -15,8 +15,6 @@ import 'aos/dist/aos.css'
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-console.log(import.meta.env)
-console.log('hello')
 // Firebase configuration using environment variables/
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || import.meta.env.VUE_APP_FIREBASE_API_KEY,
