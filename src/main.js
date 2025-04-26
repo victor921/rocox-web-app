3a849ddd80af4b00b6ae5ca6dea8c64dae47821c
victor921 <victor.olazabal@knights.ucf.edu>
2025-04-26 13:47:20 -0400

    test

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 console.log(import.meta.env)
-// Firebase configuration using environment variables
+// Firebase configuration using environment variables/
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || import.meta.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || import.meta.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
